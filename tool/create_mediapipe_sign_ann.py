98da29d38e7a54eb6cbbf4d0a4ebde2a6b522a90
hyemin3656 <hyemin579950@gmail.com>
2026-06-02 04:34:45 +0900

    create face ann

--- tool/create_mediapipe_sign_ann.py
+++ tool/create_mediapipe_sign_ann.py
@@ -5,28 +5,18 @@ from pathlib import Path
 
 import numpy as np
 
-ROOT_DIR = Path("../../runyourai/ksl/holistic_results")
-OUT_PKL = Path("../../runyourai/ksl/mediapipe_sign_3d.pkl")
+ROOT_DIR = Path("../dataset/cropped_holistic_results")
+OUT_PKL = Path("../dataset/cropped_holistic_results/mediapipe_sign_3d.pkl")
 
 RANDOM_SEED = 42
 
-POSE_FILE = "pose_33.npy"
-LEFT_HAND_FILE = "left_hand_21.npy"
-RIGHT_HAND_FILE = "right_hand_21.npy"
-
 # 설정
 NUM_POSE = 23       # pose 0~22
+NUM_FACE = 468
 NUM_HAND = 21
-NUM_NODE = 65       # 23 + 21 + 21
+NUM_NODE = 533       # 23 + 21 + 21 + 468
 NUM_PERSON = 1
 COORD_DIM = 3       # x, y, z
-
-
-def load_npy(path: Path):
-    if not path.exists():
-        return None
-    arr = np.load(path)
-    return arr
 
 
 def ensure_tvc(arr, expected_v=None, name="array"):
@@ -50,59 +40,59 @@ def ensure_tvc(arr, expected_v=None, name="array"):
     if C < COORD_DIM:
         raise ValueError(f"{name} must have at least {COORD_DIM} coordinates, got C={C}")
 
-    return arr[:, :expected_v, :COORD_DIM].astype(np.float32)
+    return arr[:, :expected_v,].astype(np.float32)
 
 
-def make_zero_hand(T):
-    return np.zeros((T, NUM_HAND, COORD_DIM), dtype=np.float32)
+# def make_zero_hand(T):
+#     return np.zeros((T, NUM_HAND, COORD_DIM), dtype=np.float32)
 
 
-def pad_or_trim_time(arr, target_T):
-    """
-    같은 sample내의 각 npy의 T가 다를 경우 target_T에 맞춘다.
-    짧으면 0 padding, 길면 자른다.
-    """
-    T, V, C = arr.shape
+# def pad_or_trim_time(arr, target_T):
+#     """
+#     같은 sample내의 각 npy의 T가 다를 경우 target_T에 맞춘다.
+#     짧으면 0 padding, 길면 자른다.
+#     """
+#     T, V, C = arr.shape
 
-    if T == target_T:
-        return arr
+#     if T == target_T:
+#         return arr
 
-    if T > target_T:
-        return arr[:target_T]
+#     if T > target_T:
+#         return arr[:target_T]
 
-    padded = np.zeros((target_T, V, C), dtype=arr.dtype)
-    padded[:T] = arr
-    return padded
+#     padded = np.zeros((target_T, V, C), dtype=arr.dtype)
+#     padded[:T] = arr
+#     return padded
 
 
-def nan_to_zero_with_score(arr):
-    """
-    NaN이 있는 keypoint는 좌표 0, score 0 으로 설정
-    정상 keypoint는 score 1.
-    arr: #(T, NUM_POSE or NUM_HAND, COORD_DIM)
-    """
-    invalid = np.isnan(arr).any(axis=-1)  # [T, NUM_POSE] (keypoint-level)
-    score = (~invalid).astype(np.float32)
+# def nan_to_zero_with_score(arr):
+#     """
+#     NaN이 있는 keypoint는 좌표 0, score 0 으로 설정
+#     정상 keypoint는 score 1.
+#     arr: #(T, NUM_POSE or NUM_HAND, COORD_DIM)
+#     """
+#     invalid = np.isnan(arr).any(axis=-1)  # [T, NUM_POSE] (keypoint-level)
+#     score = (~invalid).astype(np.float32)
 
-    arr = np.nan_to_num(arr, nan=0.0, posinf=0.0, neginf=0.0)
-    arr[invalid] = 0.0
+#     arr = np.nan_to_num(arr, nan=0.0, posinf=0.0, neginf=0.0)
+#     arr[invalid] = 0.0
 
-    return arr.astype(np.float32), score.astype(np.float32)
+#     return arr.astype(np.float32), score.astype(np.float32)
 
-def remap_label(label):
-    #비어있는 클래스 제거한 클래스 label 반환
-    #label : 1~77
-    missing = [12, 19, 28, 33, 35, 45, 46, 53, 73, 75]
+# def remap_label(label):
+#     #비어있는 클래스 제거한 클래스 label 반환
+#     #label : 1~77
+#     missing = [12, 19, 28, 33, 35, 45, 46, 53, 73, 75]
 
-    # label보다 작은 missing 개수 세기
-    shift = sum(1 for m in missing if m < label)
+#     # label보다 작은 missing 개수 세기
+#     shift = sum(1 for m in missing if m < label)
 
-    return label - shift #1~67
+#     return label - shift #1~67
 
 def parse_subject_and_class(folder_name):
     """
     폴더명이 '피험자_클래스' 형태라고 가정.
-    ex. 19(00~19)_07(01~77)
+    ex. 19(00~19)_07(00~66)
     """
     if "_" not in folder_name:
         raise ValueError(
@@ -112,9 +102,8 @@ def parse_subject_and_class(folder_name):
 
     subject, class_name = folder_name.rsplit("_", 1)
     subject = subject.lstrip("0") or "0"
-    class_name = class_name.lstrip("0")
-    remapped_class_label = remap_label(int(class_name))
-    return subject, remapped_class_label - 1 #0~66
+    class_name = class_name.lstrip("0") or "0"
+    return subject, int(class_name)  #0~66
 
 
 def build_annotations():
@@ -122,48 +111,60 @@ def build_annotations():
 
     annotations = []
     split = {}
+    frame_lengths = []   
+
     for split_dir in split_dirs:
         split_name = split_dir.name
         split[split_name] = []
-        sample_dirs = sorted([p for p in split_dir.iterdir() if p.is_dir()])
-        for sample_dir in sample_dirs: #each sample directory
-            frame_dir = sample_dir.name #{subject_id}_{class_id}
-            subject, class_name = parse_subject_and_class(frame_dir)
+        for npz_path in sorted(split_dir.rglob("*.npz")):
+            file_name = npz_path.stem      # 00_00
+            subject, class_name = parse_subject_and_class(file_name)
 
-            pose = load_npy(sample_dir / POSE_FILE) 
-            left = load_npy(sample_dir / LEFT_HAND_FILE)
-            right = load_npy(sample_dir / RIGHT_HAND_FILE)
+            npz = np.load(npz_path)
+            pose = npz["pose"]
+            face = npz["face"]
+            left_hand = npz["left_hand"]
+            right_hand = npz["right_hand"]
 
-            if pose is None:
-                print(f"[SKIP] pose.npy missing: {sample_dir}")
-                continue
-
-            pose = ensure_tvc(pose, expected_v=NUM_POSE, name=f"{frame_dir}/pose") #(T, NUM_POSE, COORD_DIM)
-
+            pose = ensure_tvc(pose, expected_v=NUM_POSE, name=f"{file_name}[pose]") #(T, NUM_POSE, 4)
+            face = ensure_tvc(face, expected_v=NUM_FACE, name=f"{file_name}[face]") #(T, NUM_FACE, 4)
+            left = ensure_tvc(left_hand, expected_v=NUM_HAND, name=f"{file_name}[left hand]") #(T, NUM_HAND, 4)
+            right = ensure_tvc(right_hand, expected_v=NUM_HAND, name=f"{file_name}/[right hand]") #(T, NUM_HAND, 4)
             T = pose.shape[0]
+            frame_lengths.append(T)   # 추가
 
-            if left is None:
-                left = make_zero_hand(T)
-            else:
-                left = ensure_tvc(left, expected_v=NUM_HAND, name=f"{frame_dir}/left hand") #(T, NUM_HAND, COORD_DIM)
-                left = pad_or_trim_time(left, T) #pose의 프레임 개수 기준 (동적 crop 시 hand 기준으로 수정 필요)
+            arrays = {
+                "pose": pose,
+                "face": face,
+                "left_hand": left,
+                "right_hand": right,
+            }
+            # 1. NaN 있으면 에러
+            for name, arr in arrays.items():
+                if np.isnan(arr).any():
+                    raise ValueError(f"{file_name}[{name}] contains NaN")
 
-            if right is None:
-                right = make_zero_hand(T)
-            else:
-                right = ensure_tvc(right, expected_v=NUM_HAND, name=f"{frame_dir}/right hand") #(T, NUM_HAND, COORD_DIM)
-                right = pad_or_trim_time(right, T) #pose의 프레임 개수 기준
+            # 2. T가 모두 같지 않으면 에러
+            T_values = {name: arr.shape[0] for name, arr in arrays.items()}
 
-            pose, pose_score = nan_to_zero_with_score(pose)
-            left, left_score = nan_to_zero_with_score(left)
-            right, right_score = nan_to_zero_with_score(right)
+            if len(set(T_values.values())) != 1:
+                raise ValueError(
+                    f"{file_name} has inconsistent T: "
+                    + ", ".join([f"{name}={T}" for name, T in T_values.items()]))
 
-            # [T, 65, 3]
-            keypoint = np.concatenate([pose, left, right], axis=1)
+            keypoints = {}
+            scores = {}
+
+            for name, arr in arrays.items():
+                keypoints[name] = arr[..., :-1]  # (T, V, C-1)
+                scores[name] = arr[..., -1]      # (T, V)
+
+            # [T, 533, 3]
+            keypoint = np.concatenate([keypoints['pose'], keypoints['face'], keypoints['left_hand'], keypoints['right_hand']], axis=1)
             input_keypoint = keypoint[None, ...].astype(np.float32) # [M, T, V, C]
 
-            # [T, 65]
-            keypoint_score = np.concatenate([pose_score, left_score, right_score], axis=1)
+            # [T, 533]
+            keypoint_score = np.concatenate([scores['pose'], scores['face'], scores['left_hand'], scores['right_hand']], axis=1)
             input_keypoint_score = keypoint_score[None, ...].astype(np.float32)  # [M, T, V]
 
             assert keypoint.shape == (T, NUM_NODE, COORD_DIM), keypoint.shape
@@ -171,26 +172,24 @@ def build_annotations():
 
             annotations.append(
                 {
-                    "frame_dir": frame_dir,
+                    "frame_dir": file_name,
                     "label": class_name, # 0~66
                     "total_frames": T,
                     "keypoint": input_keypoint, 
                     "keypoint_score": input_keypoint_score
-                    # 3D skeleton에서는 필수는 아니지만, missing 정보를 보존하고 싶으면 같이 넣어둘 수 있음
-                    # 단, 모델이 기본적으로 score를 feature로 쓰지는 않음
                 }
             )
 
-            split[split_name].append(frame_dir)
+            split[split_name].append(file_name)
         print(f"{split_name} 완료")
 
-    return annotations, split
+    return annotations, split, frame_lengths
 
 
 def main():
     OUT_PKL.parent.mkdir(parents=True, exist_ok=True)
 
-    annotations, split = build_annotations()
+    annotations, split, frame_lengths = build_annotations()
 
     ann_file = {
         "split": split,
@@ -202,9 +201,13 @@ def main():
 
     print(f"Saved annotation file to: {OUT_PKL}")
     print(f"Num samples: {len(annotations)}")
-
+    # frame 통계
+    print(f"Average frames: {np.mean(frame_lengths):.2f}")
+    print(f"Min frames: {np.min(frame_lengths)}")
+    print(f"Max frames: {np.max(frame_lengths)}")
+    print(f"Median frames: {np.median(frame_lengths):.2f}")
     # shape 확인용
-    first = annotations[0]
+    first = annotations[6]
     print("Example:")
     print(" frame_dir:", first["frame_dir"])
     print(" label:", first["label"])
